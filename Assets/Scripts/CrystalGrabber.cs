1f1433a729295fa0587801666151293c62981ec4
Markku Inkinen <markku.inkinen96@gmail.com>
2023-11-29 23:31:08 +0200

    magnet done
    the magnet now works as intended

--- Assets/Scripts/CrystalGrabber.cs
+++ Assets/Scripts/CrystalGrabber.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class CrystalGrabber : MonoBehaviour
 {
-    public CircleCollider2D playerMagnet;
+
     public GameObject spriteObject;
     public CircleCollider2D circleCollider;
-    public GameObject playerMagnetIcon;
 
+    public GameObject playerMagnetIcon;
+    public CircleCollider2D playerMagnet;
+    public PlayerController player;
     public float timer;
+    
     [SerializeField]
     private bool magnetOn;
 
     void Start()
     {
+        player = FindObjectOfType<PlayerController>();
+
+        playerMagnet = player.transform.Find("PlayerMagnet").GetComponent<CircleCollider2D>();
+        Transform middleOfPlayer = player.transform.Find("MiddleOfPlayer");
+        playerMagnetIcon = middleOfPlayer.Find("MagnetIcon").gameObject;
+        //if (player != null)
+        //{
+        //    playerMagnet = player.transform.Find("PlayerMagnet").GetComponent<CircleCollider2D>();
+
+        //    if (playerMagnet != null)
+        //    {
+        //        Transform middleOfPlayer = player.transform.Find("MiddleOfPlayer");
+
+        //        if (middleOfPlayer != null)
+        //        {
+        //            playerMagnetIcon = middleOfPlayer.Find("MagnetIcon").gameObject;
+        //        }
+        //        else
+        //        {
+        //            Debug.LogError("MiddleOfPlayer not found!");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("PlayerMagnet GameObject not found on the player!");
+        //    }
+        //}
+        //else
+        //{
+        //    Debug.LogError("Player object not found!");
+        //}
     }
 
 
